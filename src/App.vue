25c23d09714502eedc0db1bc06e1613363fcd5f1
Adlok53 <01mikinini@gmail.com>
2024-12-13 03:09:52 +0300

    MyNotes application

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-fluid">
+        <h1>App</h1>
+        <my-notes v-for='note in notes' v-bind:key="note" v-bind:oneNote="note"></my-notes>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyNotes from "./MyNotes"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            notes: [
+                {title: "Почистить зубы", message: "One"},
+                {title: "Покушать", message: "Two"},
+                {title: "Сходить на работу", message: "Three"}
+            ]
+        }
+    },
+    components: {
+        MyNotes
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/MyNotes.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Заметка: {{ oneNote.title }}</h2>
+        <strong>Прочитано:</strong>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["oneNote"],
+    data() {
+        return {}
+    }
+}
+</script>
